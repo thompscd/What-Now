ca775669309cba3c8c731ac996bbd90b217bf0be
Christopher Thompson <christopherthompsonuk@gmail.com>
2021-07-17 22:55:42 +0100

    Added a default login/password to the pupil login screen, Helps with development & testing but will need to be removed at the end.

---
+++ What Next/MoodViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MoodViewController.swift
+//  What Next
+//
+//  Created by Lorraine Thompson on 16/07/2021.
+//
+
+import UIKit
+
+class MoodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // automatically run the Mood Popup view
+        //print ("About to call the mood popup view controller");
+        //let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MoodViewController") as UIViewController
+        //self.present(viewController, animated: false, completion: nil)
+        //self.present(viewController, animated: false, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
