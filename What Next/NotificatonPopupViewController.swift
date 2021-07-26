7c5fc4a7cacedf3bfd14594dd7dca7b1551263b4
chris <chris@thompson22.co.uk>
2021-07-26 17:51:00 +0100

    blank popup with buttons displayed

---
+++ What Next/NotificatonPopupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotificatonPopupViewController.swift
+//  What Next
+//
+//  Created by Andy Thompson on 26/07/2021.
+//
+
+import UIKit
+
+class NotificatonPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
