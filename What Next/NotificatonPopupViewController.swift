06515ebfc0a989b1e55271aa0c2f4f5b95cf0cec
chris <chris@thompson22.co.uk>
2021-07-26 21:14:43 +0100

    changed NotificatonPopupViewController so that it has a func to open the database

--- What Next/NotificatonPopupViewController.swift
+++ What Next/NotificatonPopupViewController.swift
@@ -9,13 +9,52 @@ import UIKit
 
 class NotificatonPopupViewController: UIViewController {
 
+    var whatnextDB = FMDatabase()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        openDatabase()
+        displayUnreadNotifications()
+        
     }
     
-
+    // scroll through each unread nofitication and display the details
+    func displayUnreadNotifications () {
+        
+        print ("In NotificatonPopupViewController")
+        
+        // grey out the Next key and wait for Cancel button
+        
+    }
+    
+    func openDatabase () {
+        //////////////////////////////////////////
+        // THIS IS A HACK. THIS IS CODE COPIED FROM ViewController.swift. NEED TO CHANGE SO THAT THE whatnextDB variable is passed between controllers !!!!!
+        var databasePath = String();
+        let filemgr = FileManager.default
+        let dirPaths = filemgr.urls(for: .documentDirectory,
+                       in: .userDomainMask)
+        databasePath = dirPaths[0].appendingPathComponent("whatnext.db").path
+        whatnextDB = FMDatabase(path: databasePath as String)
+        if whatnextDB == nil {
+            print("Error PupilLoginViewController: whatnextDB is nil, \(whatnextDB.lastErrorMessage())")
+        } else {
+            print ("PupilLoginViewControl: database not nil")
+            if (whatnextDB.open()) {
+                print ("PupilLoginViewControl: database is open")
+            } else {
+                print("Error PupilLoginViewController: whatnextDB not open, \(whatnextDB.lastErrorMessage())")
+            }
+        }
+        // END OF HACK CODE !!!!!!!
+        /////////////////////////////////////////////////
+        
+    } // openDatabase
+    
+ 
     /*
     // MARK: - Navigation
 
