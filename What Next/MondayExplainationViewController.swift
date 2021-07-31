d5533808237f81ed842c1711be2f47d7b0630d48
thompscd <christopherthompsonuk@gmail.com>
2021-07-31 12:22:31 +0100

    Monday Explaination and Test Screen Added.

---
+++ What Next/MondayExplainationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MondayExplainationViewController.swift
+//  What Next
+//
+//  Created by Christopher Thompson on 31/07/2021.
+//
+
+import UIKit
+
+class MondayExplainationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func returnToPreviousScreen(_ sender: UIButton) {
+        performSegueToReturnBack()
+    }
+    
+}
