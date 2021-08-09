f12b8c14dd895c2355662916e4e93b89ae7a988c
chris <chris@thompson22.co.uk>
2021-08-09 16:29:53 +0100

    Pupil postings feature now complete

--- What Next/TeacherViewController.swift
+++ What Next/TeacherViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class TeacherViewController: UIViewController {
     @IBOutlet weak var teacherNameLabel: UILabel!
-    
+    @IBOutlet weak var pupilPostingsTextView: UITextView!
+    @IBOutlet weak var pupilPostingsLabel: UILabel!
     var menuOut = false;
     
     override func viewDidLoad() {
@@ -21,6 +22,8 @@ class TeacherViewController: UIViewController {
         print ("In TeacherViewController !!") //DEBUG
         let teacherName = GlobalVar.suffix+" "+GlobalVar.lastname;
         teacherNameLabel.text = teacherName;
+        
+        displayPupilPostings ();
     }
     
     @IBOutlet weak var trailing: NSLayoutConstraint!
@@ -46,80 +49,85 @@ class TeacherViewController: UIViewController {
         }
     } // TeacherMenuButton
     
-    /* DEBUG to complete
     // extract pupil postings from the database and display in the textField
     func displayPupilPostings () {
         
-        // sql query to extract all the comments
-        let querySQL = "SELECT loginname, activity, comment, date, moodlevel FROM COMMENTS;"
-        var results:FMResultSet? = GlobalVar.whatNextDB.executeQuery(querySQL, withArgumentsIn:[]);
-        while results?.next()==true {
-            print ("comment found");""
-            let loginname : String = results?.string(forColumn:"loginname") ?? "";
-            let activity : String = results?.string(forColumn:"activity") ?? "";
-            let comment : String = results?.string(forColumn:"comment") ?? "";
-            let date : String = results?.string(forColumn:"date") ?? "";
-            let moodlevel : Int = results?.long(forColumn:"moodlevel") ?? 0;
+        if GlobalVar.pupilPostingsUsername != "" {
+            
+            // update title label to add pupil username
+            var updateTitle :String = "";
+            updateTitle = pupilPostingsLabel.text ?? ""
+            updateTitle = updateTitle + " - " + GlobalVar.pupilPostingsUsername
+            pupilPostingsLabel.text = updateTitle;
+            print ("updatedTitle = ", updateTitle)
+            
+            // sql query to extract all the comments for chosen pupil
+            let querySQL = "SELECT loginname, recipient, activity, comment, date, moodlevel FROM COMMENTS WHERE recipient = '\(GlobalVar.pupilPostingsUsername)';"
+            var results:FMResultSet? = GlobalVar.whatNextDB.executeQuery(querySQL, withArgumentsIn:[]);
+            while results?.next()==true {
+                print ("comment found");
+                let loginname : String = results?.string(forColumn:"loginname") ?? "";
+                let activity : String = results?.string(forColumn:"activity") ?? "";
+                let comment : String = results?.string(forColumn:"comment") ?? "";
+                let date : String = results?.string(forColumn:"date") ?? "";
+                let moodlevel : Int = results?.long(forColumn:"moodlevel") ?? 0;
 
-            // check if the comment is from teacher or pupil
-            let querySQL2 = "SELECT suffix, lastname FROM TEACHER WHERE loginname = '\(loginname)';"
-            let results2:FMResultSet? = GlobalVar.whatNextDB.executeQuery(querySQL2, withArgumentsIn:[]);
-            if results2?.next()==true {
-                // teacher comment
-                print ("Teacher comment!!")
-                let suffix : String = results2?.string(forColumn:"suffix") ?? "";
-                let lastname : String = results2?.string(forColumn:"lastname") ?? "";
-                // Display on Comments TextView
-                CommentsTextView.insertText ("\n\n");
-                CommentsTextView.insertText (suffix+" "+lastname);
-                CommentsTextView.insertText ("\nClassroom Teacher");
-                CommentsTextView.insertText ("\n\n"+"\""+comment+"\"");
-                CommentsTextView.insertText ("\n\n"+"Date: "+date);
-                CommentsTextView.insertText ("\n\n===========================");
-            }
-            else {
-                // check if the usernane is in pupil table
-                
-                // pupil comment
-                print ("Pupil comment!!")
-                let querySQL3 = "SELECT firstname, lastname FROM PUPIL WHERE loginname = '\(loginname)';"
-
-                let results3:FMResultSet? = GlobalVar.whatNextDB.executeQuery(querySQL3, withArgumentsIn:[]);
-                if results3?.next()==true {
-                    let firstname : String = results3?.string(forColumn:"firstname") ?? "";
-                    let lastname : String = results3?.string(forColumn:"lastname") ?? "";
+                // check if the comment is from teacher or pupil
+                let querySQL2 = "SELECT suffix, lastname FROM TEACHER WHERE loginname = '\(loginname)';"
+                let results2:FMResultSet? = GlobalVar.whatNextDB.executeQuery(querySQL2, withArgumentsIn:[]);
+                if results2?.next()==true {
+                    // teacher comment
+                    print ("Teacher comment!!")
+                    let suffix : String = results2?.string(forColumn:"suffix") ?? "";
+                    let lastname : String = results2?.string(forColumn:"lastname") ?? "";
                     // Display on Comments TextView
-                    CommentsTextView.insertText ("\n\n");
-                    CommentsTextView.insertText (firstname+" "+lastname);
-                    CommentsTextView.insertText ("\nPupil");
-                    if activity != "" {
-                        CommentsTextView.insertText ("\n"+activity);
-                    }
-                    CommentsTextView.insertText ("\n\n"+"\""+comment+"\"");
+                    pupilPostingsTextView.insertText ("\n"+suffix+" "+lastname);
+                    pupilPostingsTextView.insertText ("\nClassroom Teacher");
+                    pupilPostingsTextView.insertText ("\n\n"+"\""+comment+"\"");
+                    pupilPostingsTextView.insertText ("\n\n"+"Date: "+date);
+                    pupilPostingsTextView.insertText ("\n\n===========================\n");
+                }
+                else {
+                    // check if the username is in pupil table
+                
+                    // pupil comment
+                    print ("Pupil comment!!")
+                    let querySQL3 = "SELECT firstname, lastname FROM PUPIL WHERE loginname = '\(loginname)';"
 
-                    if (moodlevel == GlobalVar.moodlevel_none) {
-                        // ignore - no mood level selected by the pupil
-                    } else if (moodlevel == GlobalVar.moodlevel_happy) {
-                        // Happy
-                        CommentsTextView.insertText ("\n\nMood level : Happy");
-                    } else if (moodlevel == GlobalVar.moodlevel_ok) {
-                        // ok
-                        CommentsTextView.insertText ("\n\nMood level : ok");
-                    } else if (moodlevel == GlobalVar.moodlevel_sad) {
-                        // sad
-                        CommentsTextView.insertText ("\n\nMood level : sad");
+                    let results3:FMResultSet? = GlobalVar.whatNextDB.executeQuery(querySQL3, withArgumentsIn:[]);
+                    if results3?.next()==true {
+                        let firstname : String = results3?.string(forColumn:"firstname") ?? "";
+                        let lastname : String = results3?.string(forColumn:"lastname") ?? "";
+                        // Display on Comments TextView
+                        pupilPostingsTextView.insertText ("\n"+firstname+" "+lastname);
+                        pupilPostingsTextView.insertText ("\nPupil");
+                        if activity != "" {
+                            pupilPostingsTextView.insertText ("\n"+activity);
+                        }
+                        pupilPostingsTextView.insertText ("\n\n"+"\""+comment+"\"");
+
+                        if (moodlevel == GlobalVar.moodlevel_none) {
+                            // ignore - no mood level selected by the pupil
+                        } else if (moodlevel == GlobalVar.moodlevel_happy) {
+                            // Happy
+                            pupilPostingsTextView.insertText ("\n\nMood level : Happy");
+                        } else if (moodlevel == GlobalVar.moodlevel_ok) {
+                            // ok
+                            pupilPostingsTextView.insertText ("\n\nMood level : ok");
+                        } else if (moodlevel == GlobalVar.moodlevel_sad) {
+                            // sad
+                            pupilPostingsTextView.insertText ("\n\nMood level : sad");
+                        }
+                        pupilPostingsTextView.insertText ("\n"+"Date: "+date);
+                        pupilPostingsTextView.insertText ("\n\n===========================\n");
+                    } else {
+                        // username is not in TEACHER or PUPIL table - report an error
                     }
-                    CommentsTextView.insertText ("\n\n"+"Date: "+date);
-                    CommentsTextView.insertText ("\n\n===========================");
-                } else {
-                    // username is not in TEACHER or PUPIL table - report an error
                 }
             }
+        } // pupil name had been specified by teacher
 
-        }
     }  // displayPupilPostings
- DEBUG */
-    
     
     
     /*
