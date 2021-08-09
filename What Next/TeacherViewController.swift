6eff7d695ca8caedf3050cc7651283b99253759c
chris <chris@thompson22.co.uk>
2021-08-09 15:02:21 +0100

    Added text view to main teacher screen. Now need to add recipient field to comments database table.

--- What Next/TeacherViewController.swift
+++ What Next/TeacherViewController.swift
@@ -44,7 +44,83 @@ class TeacherViewController: UIViewController {
         }) { (animationComplete) in
             print("The animation is complete!")
         }
-    }
+    } // TeacherMenuButton
+    
+    /* DEBUG to complete
+    // extract pupil postings from the database and display in the textField
+    func displayPupilPostings () {
+        
+        // sql query to extract all the comments
+        let querySQL = "SELECT loginname, activity, comment, date, moodlevel FROM COMMENTS;"
+        var results:FMResultSet? = GlobalVar.whatNextDB.executeQuery(querySQL, withArgumentsIn:[]);
+        while results?.next()==true {
+            print ("comment found");""
+            let loginname : String = results?.string(forColumn:"loginname") ?? "";
+            let activity : String = results?.string(forColumn:"activity") ?? "";
+            let comment : String = results?.string(forColumn:"comment") ?? "";
+            let date : String = results?.string(forColumn:"date") ?? "";
+            let moodlevel : Int = results?.long(forColumn:"moodlevel") ?? 0;
+
+            // check if the comment is from teacher or pupil
+            let querySQL2 = "SELECT suffix, lastname FROM TEACHER WHERE loginname = '\(loginname)';"
+            let results2:FMResultSet? = GlobalVar.whatNextDB.executeQuery(querySQL2, withArgumentsIn:[]);
+            if results2?.next()==true {
+                // teacher comment
+                print ("Teacher comment!!")
+                let suffix : String = results2?.string(forColumn:"suffix") ?? "";
+                let lastname : String = results2?.string(forColumn:"lastname") ?? "";
+                // Display on Comments TextView
+                CommentsTextView.insertText ("\n\n");
+                CommentsTextView.insertText (suffix+" "+lastname);
+                CommentsTextView.insertText ("\nClassroom Teacher");
+                CommentsTextView.insertText ("\n\n"+"\""+comment+"\"");
+                CommentsTextView.insertText ("\n\n"+"Date: "+date);
+                CommentsTextView.insertText ("\n\n===========================");
+            }
+            else {
+                // check if the usernane is in pupil table
+                
+                // pupil comment
+                print ("Pupil comment!!")
+                let querySQL3 = "SELECT firstname, lastname FROM PUPIL WHERE loginname = '\(loginname)';"
+
+                let results3:FMResultSet? = GlobalVar.whatNextDB.executeQuery(querySQL3, withArgumentsIn:[]);
+                if results3?.next()==true {
+                    let firstname : String = results3?.string(forColumn:"firstname") ?? "";
+                    let lastname : String = results3?.string(forColumn:"lastname") ?? "";
+                    // Display on Comments TextView
+                    CommentsTextView.insertText ("\n\n");
+                    CommentsTextView.insertText (firstname+" "+lastname);
+                    CommentsTextView.insertText ("\nPupil");
+                    if activity != "" {
+                        CommentsTextView.insertText ("\n"+activity);
+                    }
+                    CommentsTextView.insertText ("\n\n"+"\""+comment+"\"");
+
+                    if (moodlevel == GlobalVar.moodlevel_none) {
+                        // ignore - no mood level selected by the pupil
+                    } else if (moodlevel == GlobalVar.moodlevel_happy) {
+                        // Happy
+                        CommentsTextView.insertText ("\n\nMood level : Happy");
+                    } else if (moodlevel == GlobalVar.moodlevel_ok) {
+                        // ok
+                        CommentsTextView.insertText ("\n\nMood level : ok");
+                    } else if (moodlevel == GlobalVar.moodlevel_sad) {
+                        // sad
+                        CommentsTextView.insertText ("\n\nMood level : sad");
+                    }
+                    CommentsTextView.insertText ("\n\n"+"Date: "+date);
+                    CommentsTextView.insertText ("\n\n===========================");
+                } else {
+                    // username is not in TEACHER or PUPIL table - report an error
+                }
+            }
+
+        }
+    }  // displayPupilPostings
+ DEBUG */
+    
+    
     
     /*
     // MARK: - Navigation
