8c92bd0c353c90f8ac8f0ed5a6e10f943c9a78f1
chris <chris@thompson22.co.uk>
2021-08-18 16:35:43 +0100

    added tests for What_NextUITests_LiteracyViewController.swift

---
+++ What NextUITests/What_NextUITests_LiteracyViewController.swift
@@ -0,0 +1,54 @@
+//
+//  What_NextUITests_LiteracyViewController.swift
+//  What NextUITests
+//
+//  Created by Chris Thompson on 03/07/2021.
+//
+
+import XCTest
+
+class What_NextUITests_LiteracyViewController: XCTestCase {
+
+    // check format of Literacy View controller screen
+    func testLiteracyScreenFormat() throws {
+        
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        app/*@START_MENU_TOKEN@*/.staticTexts["Test Literacy"]/*[[".buttons[\"TestLiteracy\"].staticTexts[\"Test Literacy\"]",".staticTexts[\"Test Literacy\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.forceTapElement()
+        
+        // check title exists
+        let yourLessons = app.staticTexts["YourLessonsThisWeek"]
+        XCTAssertTrue(yourLessons.exists)
+
+        //check link for each of the days exist
+        let monday = app.buttons["Monday"]
+        XCTAssertTrue(monday.exists)
+        let tuesday = app.buttons["Tuesday"]
+        XCTAssertTrue(tuesday.exists)
+        let wednesday = app.buttons["Wednesday"]
+        XCTAssertTrue(wednesday.exists)
+        let thursday = app.buttons["Thursday"]
+        XCTAssertTrue(thursday.exists)
+        let friday = app.buttons["Friday"]
+        XCTAssertTrue(friday.exists)
+        
+        
+    } //testLiteracyScreenFormat
+    
+}
+
+/*
+extension XCUIElement {
+    func forceTapElement() {
+        if self.isHittable {
+            self.tap()
+        }
+        else {
+            let coordinate: XCUICoordinate = self.coordinate(withNormalizedOffset: CGVector(dx:0.0, dy:0.0))
+            coordinate.tap()
+        }
+    }
+}
+ */
